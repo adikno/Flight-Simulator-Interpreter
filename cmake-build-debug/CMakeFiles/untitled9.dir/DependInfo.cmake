5ac37e8646733eeffbecebe86932a920e5409526
michalabr7 <michalabr7@gmail.com>
2018-12-13 18:45:46 +0200

    adding cmake

---
+++ cmake-build-debug/CMakeFiles/untitled9.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/michal/CLionProjects/untitled9/Client.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/Client.cpp.o"
+  "/home/michal/CLionProjects/untitled9/Command.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/Command.cpp.o"
+  "/home/michal/CLionProjects/untitled9/CommandExpression.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/CommandExpression.cpp.o"
+  "/home/michal/CLionProjects/untitled9/ConnectCommand.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/ConnectCommand.cpp.o"
+  "/home/michal/CLionProjects/untitled9/DataReaderServer.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/DataReaderServer.cpp.o"
+  "/home/michal/CLionProjects/untitled9/Div.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/Div.cpp.o"
+  "/home/michal/CLionProjects/untitled9/Minus.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/Minus.cpp.o"
+  "/home/michal/CLionProjects/untitled9/Mult.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/Mult.cpp.o"
+  "/home/michal/CLionProjects/untitled9/OpenServerCommand.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/OpenServerCommand.cpp.o"
+  "/home/michal/CLionProjects/untitled9/Plus.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/Plus.cpp.o"
+  "/home/michal/CLionProjects/untitled9/VarCommand.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/VarCommand.cpp.o"
+  "/home/michal/CLionProjects/untitled9/main.cpp" "/home/michal/CLionProjects/untitled9/cmake-build-debug/CMakeFiles/untitled9.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled9.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled9.dir/main.cpp.o"
+  "CMakeFiles/untitled9.dir/Command.cpp.o"
+  "CMakeFiles/untitled9.dir/OpenServerCommand.cpp.o"
+  "CMakeFiles/untitled9.dir/DataReaderServer.cpp.o"
+  "CMakeFiles/untitled9.dir/ConnectCommand.cpp.o"
+  "CMakeFiles/untitled9.dir/Client.cpp.o"
+  "CMakeFiles/untitled9.dir/Plus.cpp.o"
+  "CMakeFiles/untitled9.dir/Minus.cpp.o"
+  "CMakeFiles/untitled9.dir/Div.cpp.o"
+  "CMakeFiles/untitled9.dir/Mult.cpp.o"
+  "CMakeFiles/untitled9.dir/CommandExpression.cpp.o"
+  "CMakeFiles/untitled9.dir/VarCommand.cpp.o"
+  "untitled9.pdb"
+  "untitled9"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
